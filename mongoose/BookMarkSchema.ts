922da9c67992cacf58efd918d347b0695d8e7318
meet mehta <meetmehta10@gmail.com>
2022-02-25 17:16:41 -0500

    Bug fix

--- mongoose/BookMarkSchema.ts
+++ mongoose/BookMarkSchema.ts
@@ -11,7 +11,7 @@ import BookMark from "../models/BookMark";
  */
 const BookMarkSchema = new mongoose.Schema<BookMark>({
     bookMarkedTuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
-    bookMarkedBy: {type: Schema.Types.ObjectId, ref: "BookMarkModel"},
+    bookMarkedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
 }, {collection: "bookmark"});
 
 export default BookMarkSchema;
