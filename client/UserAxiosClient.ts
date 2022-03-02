9883b8396dc466bca5d7c6054d73d4fc183528a2
meet mehta <meetmehta10@gmail.com>
2022-03-02 16:30:38 -0500

    User Login node code bug fix

---
+++ client/UserAxiosClient.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
+import User from "../models/User";
+// axios.defaults.baseURL = 'http://localhost:4000/api';
+axios.defaults.baseURL = 'https://cs5500-01-sp22.herokuapp.com/api';
+
+const findAllUsers = async () =>
+    await axios.get('/users');
+
+const findUserById = async (uid: string) => {
+  return await axios.get(`/users/${uid}`);
+}
+
+const createUser = async (user: User) =>
+  await axios.post('/users', user);
+
+const updateUser = async (uid: string, user: User) =>
+  await axios.put(`/users/${uid}`, user);
+
+const deleteUser = async (uid: string) =>
+  await axios.delete(`/users/${uid}`);
+
