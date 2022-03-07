127d989d0517f3c56f45d4909f7e9305fb52186a
meet mehta <meetmehta10@gmail.com>
2022-03-07 12:21:40 -0500

    bug fix

--- interfaces/TuitController.ts
+++ interfaces/TuitController.ts
@@ -9,7 +9,7 @@ export default interface TuitControllerI {
     findAllTuits (req: Request, res: Response): void;
     findAllTuitsByUser (req: Request, res: Response): void;
     findTuitById (req: Request, res: Response): void;
-    createTuit (req: Request, res: Response): void;
+    createTuitByUserByUser (req: Request, res: Response): void;
     updateTuit (req: Request, res: Response): void;
     deleteTuit (req: Request, res: Response): void;
 };
